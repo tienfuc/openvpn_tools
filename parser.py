dbab9b50988237aebb94ef4ce9dd5f47e76494e6
ctf <tienfu.chen@canonical.com>
2016-07-02 23:34:22 +0800

    added proto, port, country

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import requests
 import csv
 import StringIO
@@ -14,19 +15,39 @@ text = re.sub(r"#HostName","HostName", text_remove_1)
 
 csv_reader = csv.DictReader(StringIO.StringIO(text))
 count = 0
+default_dir = "/opt/davfs/vpn"
 
 for csv in csv_reader:
     hostname = csv["HostName"]
     ip = csv["IP"]
+    country = csv["CountryShort"]
+    session = csv["NumVpnSessions"]
+    if session:
+        if session.isdigit() and (int(session) < 2):
+            continue
+    else:
+        continue
 
     try:
         decoded = base64.b64decode(csv["OpenVPN_ConfigData_Base64"])
+
+        proto_line = re.findall(r"^proto (\w*)", decoded, re.MULTILINE)
+        if proto_line:
+            proto = proto_line[0]
+            #print proto
+
+        remote_line = re.findall(r"^remote .* (\d*)", decoded, re.MULTILINE)
+        if remote_line:
+            port = remote_line[0]
+            #print port
+
     except:
         pass
     finally:
         if( hostname and ip and decoded ):
-            filename = "%s_%s.ovpn" % (hostname, ip)
-            with open(filename, "w+") as f:
+            filename = "%s_%s_%s_%s_%s.ovpn" % (hostname, ip, port, proto, country)
+            fullname = os.path.join(default_dir, filename)
+            with open(fullname, "w+") as f:
                 count+=1
                 f.write(decoded)
 
